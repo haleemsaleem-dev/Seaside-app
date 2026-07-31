8d2ad9a4246d9df5bb23a75bf0aa5f39da3bd091
haleemasaleem-dev <haleema.devworks@gmail.com>
2026-07-31 04:49:16 -0700

    Make Company header responsive for mobile

--- src/components/Company/CompanyHeader.jsx
+++ src/components/Company/CompanyHeader.jsx
@@ -3,17 +3,20 @@ import { FaPlus } from "react-icons/fa";
 
 const CompanyHeader = ({ onAddCompany }) => {
   return (
-    <div className="flex items-end justify-between mb-5">
+    <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-4 mb-5">
+      
       <div>
-        <h1 className="text-white text-[40px] leading-none">Companies</h1>
+        <h1 className="text-white text-[40px] leading-none">
+          Companies
+        </h1>
 
         <div className="w-[200px] h-[2px] bg-white rounded-full mt-3"></div>
       </div>
 
+
       <button
         onClick={onAddCompany}
         className="
-          ml-8
           h-[48px]
           px-6
           rounded-xl
@@ -25,14 +28,19 @@ const CompanyHeader = ({ onAddCompany }) => {
           gap-3
           shadow-md
           transition
+          w-fit
+          md:ml-8
         "
       >
         <span className="w-7 h-7 rounded-full bg-white flex items-center justify-center">
           <FaPlus className="text-[#F7A62C] text-xs" />
         </span>
 
-        <span className="text-sm font-medium">Add Company</span>
+        <span className="text-sm font-medium">
+          Add Company
+        </span>
       </button>
+
     </div>
   );
 };
